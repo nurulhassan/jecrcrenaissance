5d4ae95bf8a88d138383730bba4766ac68f7071d
prasang007 <prasang00@gmail.com>
2019-03-02 00:39:44 +0530

    event-js-finished date-added-event fixed-bugs

--- events/changeImage.js
+++ events/changeImage.js
@@ -1,24 +1,22 @@
 
 var DocWidth = $(window).width()
 // var DocHeight = $(document).height()
+
 $(document).ready(() => {
-			
-		
-			$(window).load(() => {
+			// console.log(DocWidth)
 					
-					// console.log(DocWidth)
-					if(DocWidth<=768){
+					console.log(DocWidth)
+					if(DocWidth<=660){
 						$('.certificate_img').attr("src","../../elmt/frame.png")
 					}
 					else{
 						$('.certificate_img').attr("src","../../elmt/farme.png")	
 					}
-				})
 
 			})
-$(window).resize(()=> {
+$(window).on('resize',function () { 
  		var DocWidth = $(window).width()
-		if(DocWidth<=768){
+		if(DocWidth<=660){
 						$('.certificate_img').attr("src","../../elmt/frame.png")
 					}
 					else{
@@ -26,7 +24,4 @@ $(window).resize(()=> {
 					}
 
 })
-$('.nav-item').click(()=>{
-	$('.nav-link').addClass("active")
 
-	})
